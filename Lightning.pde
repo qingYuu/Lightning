0a478098fecd55b712cec2700608ecdd891e2d1f
unknown <xiyu5@697-334-Frodo.sf.k12.internal>
2015-09-01 11:38:53 -0700

    gamma

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,35 @@
+int startX = 150;
+int startY = 0;
+int endX = 150;
+int endY = 0;
+
+
 void setup()
 {
   size(300,300);
+  strokeWeight(.27);
+  //background(1,38,87);
+  //photo = loadImage(filename);
 }
 void draw()
 {
-
+  stroke(0, 0, (int)(Math.random()*255));
+  while (endY < 300) 
+  {
+  	endX = startX + (int)(Math.random()*18)-9;
+    endY = startY + (int)(Math.random()*9);
+    line(startX, startY, endX, endY);
+    startX = endX;
+    startY = endY;
+  }
 }
 void mousePressed()
 {
-
+ background(1,38,87);
+ startX = 150; 
+ startY = 0;
+ endX = 150;
+ endY = 0;
+ 
 }
 
---
+++ build-tmp/source/Lightning.java
@@ -0,0 +1,59 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
+int startX = 150;
+int startY = 0;
+int endX = 150;
+int endY = 0;
+
+
+public void setup()
+{
+  size(300,300);
+  strokeWeight(.27f);
+  //background(1,38,87);
+}
+public void draw()
+{
+  stroke(0, 0, (int)(Math.random()*255));
+  while (endY < 300) 
+  {
+  	endX = startX + (int)(Math.random()*18)-9;
+    endY = startY + (int)(Math.random()*9);
+    line(startX, startY, endX, endY);
+    startX = endX;
+    startY = endY;
+  }
+}
+public void mousePressed()
+{
+ //background(1,38,87);
+ startX = 150; 
+ startY = 0;
+ endX = 150;
+ endY = 0;
+ 
+}
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
